c7069fc7183be8113202926eb2c67021e36f6701
phunn <phunguyenngoc0166@gmail.com>
2022-09-20 10:40:03 +0700

    oke

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import * as axios from "axios";
 function App() {
+  const [name,setName] = useState("");
+  const [password,setPassword] = useState("");
+  const onChangeName = (e)=>{
+    setName(e.target.value);
+  }
+  const onChangePassword = (e) =>{
+    setPassword(e.target.value);
+  }
+  const onSubmit = async (e) =>{
+    e.preventDefault();
+    const opts = {
+      headers: {
+          'Content-Type': 'application/json'
+      },
+     
+    };
+    const _url = "http://localhost:5022" + "/auth/login";
+    var param = {
+        email : name,
+        password:password
+    }
+    try {
+      var result = await axios.post(_url,param,opts);
+      if(result.status === 200){
+        alert("dang nhap thanh cong");
+        return;
+      }
+    } catch (error) {
+      alert("dang nhap khong thanh cong");
+    }
+    
+   
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onSubmit={onSubmit}>
+       <form>
+           <input type="text" className='name-input' value={name} onChange={onChangeName}></input>
+           <input type="text" className='password-input'  value={password} onChange={onChangePassword}></input>
+           <input type = "submit" value = "Submit" />
+      
+      </form> 
+
     </div>
   );
 }
